a1f65c146592aa9500c6b5368e8e0c0c93134c80
bernardio5 <mcdonald107@gmail.com>
2020-08-08 13:30:40 -0400

    enumerator fix

--- enumerator.py
+++ enumerator.py
@@ -8,12 +8,51 @@ import cv2
 import random
 import sys
 
-from c_paths import paths
-from c_author import author
-from c_book import book
-from c_scanner import scanner
-from c_library import library
+from classes.paths import paths
+from classes.book import book
+from classes.scanner import scanner
 
+# first pass at error diagnostics: scans a book index range 
+# then outputs all of the different error messages returned.
+
+class library:
+    def __init__(self):
+        self.scanner = scanner()
+        thePaths = paths()
+        self.covers = os.listdir(thePaths.coversDir)
+        self.clips = os.listdir(thePaths.clipDir)
+        self.results = [0, 0, 0,  0, 0, 0,  0, 0, 0, 0,0,0, 0,0,0, 0]
+
+
+    # a dignostic! try reading the GB XML's & print tally of various results
+    def scanClassifier(self):
+        notDone = True
+        ctr = 1
+        self.scanner.skipTo(ctr)
+        while notDone:
+            ctr = ctr +1
+            if (ctr>1000):
+                notDone = False
+            pt = self.scanner.getNextPath()
+            if (len(pt)>0):
+                if (".delete" in pt):
+                    self.results[3]+=1  # 3:delete in fn
+                else:
+                    booky = book()
+                    booky.readGbXML(self.scanner.gbID, pt)
+                    if (not booky.langOK()): 
+                        self.results[1] +=1 # ch or cn
+                    else:
+                        res = booky.scanGBDir()
+                        if (res!=0):
+                            self.results[res] +=1  # 4,5 no dir, is sound
+                        else:
+                            res = booky.scanHTML()
+                            self.results[res]+=1 
+            else: 
+                notDone = False
+        for i in range(0,16):
+            print(i, ":", self.results[i])
 
 
 print("defined")
